7c9eb7734ebea4b059bc2bb943a33aeae2cd8339
ashu234251 <110993417+ashu234251@users.noreply.github.com>
2022-08-15 17:25:49 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,14 @@
+//4. Write a program to print the first 10 odd natural numbers
+
+#include<stdio.h>
+int main()
+{
+    int i,x=1;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d ",x);
+        x=x+2;
+    }
+   
+    return 0;
+}
